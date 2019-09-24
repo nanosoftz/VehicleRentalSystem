870892730720eb9250296d51d8f0fa7404ddbfa9
Collins Uba <uba.collins29@gmail.com>
2019-09-24 07:38:31 +0400

    Vehicle Rental System 0.01 Version

---
+++ VehicleRentalApplication/src/test/java/com/nanosoftz/VehicleRentalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nanosoftz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehicleRentalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
